0044746652be2bae66443b05c6fda54a02621fc7
mushtariyibodillayeva <dilfuzaxamrayeva53@gmail.com>
2022-12-15 16:29:44 +0500

    data

---
+++ main.js
@@ -0,0 +1,6 @@
+const data1 = new Date();
+const data2 = new Date();
+const data3 = new Date();
+document.getElementById("demo1").innerHTML = data1.getUTCDate();
+document.getElementById("demo2").innerHTML = data2.getMonth();
+document.getElementById("demo3").innerHTML = data3.getFullYear();
